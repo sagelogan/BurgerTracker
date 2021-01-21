dff19be3e27bb7135e9c11e74d7ba29d75057741
Logan Sage <lvdsage@gmail.com>
2021-01-20 17:41:10 -0700

    created server.js to start connection

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+let express = require("express");
+
+let PORT = process.env.PORT || 8000;
+
+let app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
+app.set("view engine", "handlebars");
+
+let routes = require("./controller/burgerController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
